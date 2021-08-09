943109310fce63093efb85f102e610b37cda7087
yuanxinxin <yuanxinxin@111.com.cn>
2021-08-09 17:10:45 +0800

    init tpl

---
+++ src/plugin/toast/toast.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="vue-toast-wraper" v-show="localShow">
+        {{msg}}
+    </div>
+</template>
+<script>
+export default {
+    name:'toast',
+    props:{
+        msg:{
+            default:"",
+            type:String
+        },
+        isShow:{
+            default:false,
+            type:Boolean
+        }
+    },
+    data (){
+        return {
+            localShow: this.isShow
+        }
+    },
+    mounted(){
+        if(this.isShow){
+            setTimeout(() => {
+                this.localShow = false
+            },2500);
+        }
+    }
+}
+</script>
+<style scoped>
+.vue-toast-wraper{
+    background: rgba(0, 0, 0, 0.6);
+    color: #fff;
+    font-size: 30px;
+    padding: 10px;
+    border-radius:12px;
+    display: -webkit-box;
+    -webkit-box-pack: center;
+    -webkit-box-align: center;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    z-index: 2000;
+    -webkit-transform: translateY(-50%);
+    transform: translateY(-50%);
+    -webkit-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+</style>
